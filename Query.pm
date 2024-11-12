83895ac25326c1776c43c4c4f0bc21843bd109ab
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-11-12 09:03:22 +0100

    Prepare for 0.05 version

--- Query.pm
+++ Query.pm
@@ -7,7 +7,7 @@ use Class::Utils qw(set_params);
 use Error::Pure qw(err);
 use Scalar::Util qw(blessed);
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # Constructor.
 sub new {
@@ -397,6 +397,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
