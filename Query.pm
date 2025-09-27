d40da94a06b77bdd5e52d3337100913c4b58a470
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-09-27 11:05:10 +0200

    Add support for query on sitelink

--- Query.pm
+++ Query.pm
@@ -76,9 +76,14 @@ sub query_item {
 	} elsif ($query_string =~ m/^label:?([\w\-]+)?$/ms) {
 		return $self->_query_text($item, $1, 'labels');
 
+	# Sitelink.
+	} elsif ($query_string =~ m/^sitelink:?([\w\-]+)?$/ms) {
+		return $self->_query_sitelink($item, $1);
+
 	# Statement(s)
 	} elsif ($query_string =~ m/^statement:(P\d+)$/ms) {
 		return $self->_query_property_statement($item, $1);
+
 	} else {
 		err "Unsupported query string '$query_string'.";
 	}
@@ -187,6 +192,21 @@ sub _query_sense {
 	return wantarray ? @values : $values[0];
 }
 
+sub _query_sitelink {
+	my ($self, $item, $lang) = @_;
+
+	my $sitelink_exp = $lang.'wiki';
+	my $ret;
+	foreach my $sitelink (@{$item->sitelinks}) {
+		if ($sitelink->site eq $sitelink_exp) {
+			$ret = $sitelink;
+			last;
+		}
+	}
+
+	return $ret ? $ret->title : undef;
+}
+
 sub _query_text {
 	my ($self, $item, $lang, $method) = @_;
 
@@ -289,6 +309,10 @@ For description value.
 =item label:.*
 
 For label value.
+
+=item sitelink:.*
+
+For sitelink.
 
 =item statement:P\d+
 
